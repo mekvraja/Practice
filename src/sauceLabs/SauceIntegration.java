8f27b68c493e85cd08b603a50a60021fc2b206b1
284939 <284939@PC324216.cts.com>
2016-05-09 12:12:27 +0530

    dasd

---
+++ src/sauceLabs/SauceIntegration.java
@@ -0,0 +1,13 @@
+package sauceLabs;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SauceIntegration {
+
+	public static void main(String[] args) {
+
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability(capabilityName, value);
+	}
+
+}
